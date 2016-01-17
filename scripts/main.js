2bc1aebdbdf039e78501945cd5f41430216e6d5d
Jaydieboy <hecktwin@gmail.com>
2016-01-16 18:26:12 -0800

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,23 @@
-//Edit when time permits.
+$(document).ready(function() {
+  var timer, count = 0,
+    time = 10,
+    donation = 500;
+
+  function transition() {
+    clearTimeout(timer);
+    $('p').html('$' + count++);
+    
+		if (count >= (donation - 7)) {
+      time = time + 10;
+    } else if (count >= (donation - 20)) {
+      time = time + 5;
+    } else if (count >= (donation - 50)) {
+      time = 50;
+    };
+
+    if (count <= donation) {
+      timer = setTimeout(transition, time);
+    };
+  }
+  setTimeout(transition, time);
+});
