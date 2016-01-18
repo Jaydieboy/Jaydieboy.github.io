2ccd7f9df7c5dc650914f4d45c97d207c80e2ff9
Jaydieboy <hecktwin@gmail.com>
2016-01-17 22:36:15 -0800

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,32 +1,29 @@
-$(document).ready(function() {
-  //****CHANGE THESE VALUES WHEN UPDATING! (keep at top)****
-  var donation = 710,
-    updateDate = "9:10 pm 1/17/2016";
-  
-  //****Donation Count Up****
-  var timer,
-    count = 0,
-    time = 10;
+$(document).ready(function () {
+    var donation = 710,
+            updateDate = "9:10 pm 1-17-2016",
+            gofundme = 410;
 
-  function transition() {
-    clearTimeout(timer);
-    $('h3').html('$' + count++);
+    //****Draw Progress Bar****
+    setTimeout(function(){ progress2(Math.ceil(100 * (gofundme / 1300)), $('#progressBar'));}, 500);
+    progress(Math.ceil(100 * (donation / 1300)), $('#progressBar'));
+    
+    function progress(percentdon, $element) {
+        var progressBarWidth = percentdon * $element.width() / 100;
+        $element.find('#total').animate({width: progressBarWidth}, 500).html("$" + donation);
+    }
+    function progress2(percentgo, $element) {
+        var progressBarWidth = percentgo * $element.width() / 100;
+        $element.find('#gofundme').animate({width: progressBarWidth}, 500).html("GoFundMe $" + gofundme);
+    }
 
-    if (count >= (donation - 7)) {
-      time = time + 10;
-    } else if (count >= (donation - 20)) {
-      time = time + 5;
-    } else if (count >= (donation - 50)) {
-      time = 50;
-    };
+    //****Show plane cost****
+    $("#plane").hover(function () {
+        $("#planePrice").css("visibility", "visible");
+    }, function () {
+        $("#planePrice").css("visibility", "hidden");
+    });
 
-    if (count <= donation) {
-      timer = setTimeout(transition, time);
-    };
-  }
-  setTimeout(transition, time);
-  
-  //****Update "updateInfo" class****
-  var getNotice = $(".updateInfo").html();
-  $(".updateInfo").html(getNotice + " (Last updated: " + updateDate + ")");
+    //****Update "updateInfo" class****
+    var getNotice = $(".updateInfo").html();
+    $(".updateInfo").html(getNotice + " (Last updated: " + updateDate + ")");
 });
