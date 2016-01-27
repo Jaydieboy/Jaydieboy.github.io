de82ccc7e88f94096102bdeb231b482401a4e42c
Jaydieboy <hecktwin@gmail.com>
2016-01-26 23:31:35 -0800

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
     //****Global variables (Must be whole number)****
     var donation = 1077,
-            updateDate = "8:58 pm 1-25-2016",
+            updateDate = "11:31 pm 1-26-2016 (sorry, late)",
             gofundme = 410;
 
     $("#plane_price_mobile").hide(); 
